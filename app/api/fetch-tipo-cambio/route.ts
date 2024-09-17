f7dc566abfa75f6ab98c8fd32ce3ae051d468b3e
Walter Cordova <wcordova@beesion.com>
2024-09-17 17:23:58 -0600

    init proyect

---
+++ app/api/fetch-tipo-cambio/route.ts
@@ -0,0 +1,41 @@
+import { NextResponse } from 'next/server';
+import soap from 'soap';
+
+export async function POST(request: Request) {
+  const { fechainit } = await request.json();
+
+  if (!fechainit) {
+    return NextResponse.json({ error: 'Fecha inicial es requerida' }, { status: 400 });
+  }
+
+  const SOAP_URL = process.env.SOAP_URL;
+
+  if (!SOAP_URL) {
+    return NextResponse.json({ error: 'SOAP_URL no está definida en las variables de entorno' }, { status: 500 });
+  }
+
+  try {
+    console.log('SOAP URL:', SOAP_URL);
+
+    const client = await new Promise<any>((resolve, reject) => {
+      soap.createClient(SOAP_URL, (err, client) => {
+        if (err) reject(err);
+        else resolve(client);
+      });
+    });
+    console.log('Client:', client);
+
+    // Llamar al método del servicio SOAP
+    const [result] = await client.TipoCambioFechaInicialAsync({ fechainit });
+
+    // Procesar la respuesta SOAP
+    const tipoCambioResult = result.TipoCambioFechaInicialResult;
+    const vars = tipoCambioResult.Vars.Var;
+
+    // Devuelve la respuesta SOAP como JSON
+    return NextResponse.json({ data: vars });
+  } catch (error) {
+    const errorMessage = error instanceof Error ? error.message : 'Error desconocido';
+    return NextResponse.json({ error: 'Error al consumir el servicio SOAP', details: errorMessage }, { status: 500 });
+  }
+}
